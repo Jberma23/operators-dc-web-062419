2475f3d1f5db11b0a28d4368b7331c7a04d87a5a
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-14 18:17:40 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,10 @@
 def unsafe?(speed)
-  mph = #{speed}
-  puts "safe" if (mph <40 | mph > 60)
-  puts "unsafe" unless mph.between?(40,60);
+ speed = 70
+  # puts "safe" if (mph <40 | mph > 60)
+  puts "unsafe" unless speed.between?(40,60);
 end
 
-unsafe?(70)
+
 
 def not_safe?(speed)
 
